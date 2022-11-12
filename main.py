768388f70f2fca15d2fd39adc3c6f6d836c6f743
Duck <95023929+TheDuckvx@users.noreply.github.com>
2022-11-11 21:29:26 -0600

    bruh
    1 line fix

--- main.py
+++ main.py
@@ -42,15 +42,16 @@ async def on_ready():
     except Exception as exception:
         Bot_Management.log_error(__error__=exception)
 
+
 @client.command(name="kick")
 @commands.has_permissions(kick_members=True)
-async def kick(ctx: disnake.Message, __member__: disnake.Member, *, __reason__ = None):
+async def kick(ctx: disnake.Message, __member__: disnake.Member, *, __reason__=None):
     try:
-        __reason__ = __reason__ or "No Reason Provided!"
         if __reason__ is None:
             try:
-                __memberEmbed__ = disnake.Embed(title="Kicked", description=f"You Have Been Kicked For: {__reason__}", color=disnake.Color.red()) \
-                .set_thumbnail(url="https://th.bing.com/th/id/OIP.COYEZS1SJ4znwY6931asnAAAAA?pid=ImgDet&rs=1")
+                __memberEmbed__ = disnake.Embed(title="Kicked", description=f"You Have Been Kicked For: {__reason__}",
+                                                color=disnake.Color.red()) \
+                    .set_thumbnail(url="https://th.bing.com/th/id/OIP.COYEZS1SJ4znwY6931asnAAAAA?pid=ImgDet&rs=1")
                 await __member__.send(
                     embed=__memberEmbed__
                 )
@@ -60,24 +61,30 @@ async def kick(ctx: disnake.Message, __member__: disnake.Member, *, __reason__ =
                 await __member__.kick(
                     reason=__reason__
                 )
-                __MainEmbed__ = disnake.Embed(title=__member__, description=f"Successfully Kicked: {__member__} For: {__reason__}") \
+                __MainEmbed__ = disnake.Embed(title=__member__,
+                                              description=f"Successfully Kicked: {__member__} For: {__reason__}") \
                     .set_thumbnail(
-                        url='https://th.bing.com/th/id/OIP.COYEZS1SJ4znwY6931asnAAAAA?pid=ImgDet&rs=1'
-                    ) \
-                        .set_footer(
-                            icon_url='https://th.bing.com/th/id/OIP.-lq5B5ai5OKQVAIjC9VFqAHaDe?w=308&h=164&c=7&r=0&o=5&dpr=1.5&pid=1.7',
-                            text="𝓑𝔂 𝓥𝓟𝓢-𝓢𝓮𝓻𝓿𝓮𝓻 | 𝓒𝓸𝓭𝓲𝓷𝓰 - 𝓣𝓮𝓪𝓶"
-                        )
+                    url='https://th.bing.com/th/id/OIP.COYEZS1SJ4znwY6931asnAAAAA?pid=ImgDet&rs=1'
+                ) \
+                    .set_footer(
+                    icon_url='https://th.bing.com/th/id/OIP.-lq5B5ai5OKQVAIjC9VFqAHaDe?w=308&h=164&c=7&r=0&o=5&dpr=1.5&pid=1.7',
+                    text="𝓑𝔂 𝓥𝓟𝓢-𝓢𝓮𝓻𝓿𝓮𝓻 | 𝓒𝓸𝓭𝓲𝓷𝓰 - 𝓣𝓮𝓪𝓶"
+                )
                 await ctx.channel.send(
-                    embed = __MainEmbed__
+                    embed=__MainEmbed__
                 )
             except Exception as en:
                 Bot_Management.log_error(__error__=en)
-                __exception_embed__ = disnake.Embed(title="Exception", description="Cannot Kick The Following Member: " + __member__.mention)
+                __exception_embed__ = disnake.Embed(title="Exception",
+                                                    description="Cannot Kick The Following Member: " + __member__.mention)
                 __exception_embed__.set_thumbnail(url='https://media.giphy.com/media/cjIs6qePAqUU0/giphy.gif')
-                __exception_embed__.set_footer(icon_url='https://media.giphy.com/media/cjIs6qePAqUU0/giphy.gif', text="Check Terminal For More Details.")
+                __exception_embed__.set_footer(icon_url='https://media.giphy.com/media/cjIs6qePAqUU0/giphy.gif',
+                                               text="Check Terminal For More Details.")
                 await ctx.channel.send(
                     embed=__exception_embed__
                 )
     except Exception as exception:
         Bot_Management.log_error(__error__=exception)
+
+
+client.run("MTAzMjY5MTgxODAxOTcwMDc5Ng.Gh0p4h.vUbPvMS_7Ld1sCU902MDtUsOGzY4qiyRhRl3Qg")

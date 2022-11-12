c0dc1c945b6c05d3ac84c72e91446d3fbc4719ab
Duck <95023929+TheDuckvx@users.noreply.github.com>
2022-11-11 19:49:56 -0600

    fixed

--- Bot_Management/__init__.py
+++ Bot_Management/__init__.py
@@ -1,34 +1,10 @@
 def log_error(__error__: any):
     try:
-        if __error__ is None:
-            try:
-                with open("Log.txt", "w") as __fileEmpty__:
-                    __fileEmpty__.write(
-                        str(
-                            "No Error To Write."
-                        )
-                    )
-            except Exception as en:
-                print(
-                    str(
-                        en
-                    )
-                )
-        else:
-            pass
-        with open("Log.txt", 'w') as __newFile__:
-            __newFile__.write(
-                f"{__error__}"
-            )
-            __newFile__.close()
-        print(
-            str(
-                f"{__error__}"
-            )
-        )
+        __error__ = __error__ if __error__ is not None else "No Error To Write."
+        try:
+            with open("Log.txt", "w") as __fileEmpty__:
+                __fileEmpty__.write(f"{__error__}")
+        except Exception as en:
+            print(en)
     except Exception as en:
-        print(
-            str(
-                en
-            )
-        )
+        print(en)

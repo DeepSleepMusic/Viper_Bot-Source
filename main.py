d03e23f31efe16319912bcda7761011366694500
Hunter Mikesell <101362587+DeepSleepMusic@users.noreply.github.com>
2022-11-11 21:24:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
+"""Viper -> Discord Bot"""
+
+from subprocess import run
+
+try:
+    from urllib3 import request
+    from time import sleep
+    import random as rn
+    import disnake
+    import Bot_Management
+    from disnake.ext import commands, tasks
+    from itertools import cycle
+    import time
+
+
+except:
+    print("There Was A Error Importing The Lib(s) We Are Fixing The Problems On Our End!")
+    run(['pip', 'install', 'discord'])
+    run(['pip', 'install', 'random'])
+    run(['pip', 'install', 'time'])
+    run(['pip', 'install', 'urllib3'])
+
+client = commands.Bot(command_prefix="v!", intents=disnake.Intents.all())
+
+status = cycle(["Servers..", "You.."])
+
+
+@tasks.loop(seconds=5)
+async def changeStatus():
+    await client.change_presence(status=disnake.Status.dnd,
+                                 activity=disnake.Activity(type=disnake.ActivityType.watching, name=next(status)))
+
+
+@client.event
+async def on_ready():
+    try:
+        changeStatus.start()
+        print("Our Website: https://us-developers.org/\n"
+              "-----------------------------------------\n"
+              "Our Server: https://discord.gg/us-developers")
+
+    except Exception as exception:
+        Bot_Management.log_error(__error__=exception)
+
 @client.command(name="kick")
 @commands.has_permissions(kick_members=True)
 async def kick(ctx: disnake.Message, __member__: disnake.Member, *, __reason__ = None):
     try:
         __reason__ = __reason__ or "No Reason Provided!"
-        
         if __reason__ is None:
             try:
                 __memberEmbed__ = disnake.Embed(title="Kicked", description=f"You Have Been Kicked For: {__reason__}", color=disnake.Color.red()) \
